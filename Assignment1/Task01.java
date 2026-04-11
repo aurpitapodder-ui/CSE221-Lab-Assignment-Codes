ecdbf37e6eabf2b5b9f0fe581db51341cd627aa2
aurpitapodder-ui <aurpita.podder@g.bracu.ac.bd>
2026-04-12 00:46:45 +0600

    Update Task01.java

--- Assignment1/Task01.java
+++ Assignment1/Task01.java
@@ -1,1 +1,27 @@
 import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static final int MOD = (int)1e9 + 7;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+        for (int index = 0; index < t; index++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n%2==0){
+                pw.println(n+" is an Even number.");
+            }
+            else{
+                 pw.println(n+" is an Odd number.");
+            }
+            
+        }
+        
+
+        pw.close();
+    }
+}
+
+
